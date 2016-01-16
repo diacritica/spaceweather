133f6b6d1edf2c7389a4920b069b64de8a66854d
Pablo Ruiz Múzquiz <diacritica@gmail.com>
2016-01-16 18:10:15 +0100

    aia and loadsolarwind scripts

--- misc/scripts/aia.py
+++ misc/scripts/aia.py
@@ -1,8 +1,13 @@
-import os
+import os, sys
 import time, datetime
 
 import PIL
 from PIL import Image
+
+proj_path = "/home/elfo/GIT/spaceweather/src/spaceweather/"
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "spaceweather.settings")
+sys.path.append(proj_path)
+
 
 newwidth = 512
 newheight = 512
@@ -13,8 +18,12 @@ channels = ['171','193','211','304']
 
 for c in channels:
     filename = "{}-{}.jpg".format(c,now)
-    os.system("wget http://umbra.nascom.nasa.gov/images/latest_aia_{}.gif -O {}".format(c,filename))
+    os.system("wget http://umbra.nascom.nasa.gov/images/latest_aia_{}.gif -O output/{}/{}".format(c,c,filename))
     time.sleep(1)
-    downloadedimage = Image.open(filename).convert('RGB')
+    downloadedimage = Image.open("output/{}/{}".format(c,filename)).convert('RGB')
     downloadedimage = downloadedimage.resize((newwidth, newheight), PIL.Image.ANTIALIAS)
-    downloadedimage.save(filename)
+    downloadedimage.save("output/{}/{}".format(c,filename))
+
+
+os.chdir(proj_path)
+from core.models import *
--- misc/scripts/loadsolarwind.py
+++ misc/scripts/loadsolarwind.py
@@ -1,14 +1,28 @@
-from core.models import *
-import json
 
-myfile = open("/home/diacritica/GIT/spaceweather/misc/research/SW/plasma-7-day.json","r")
+import json
+import os, sys
+import django
+
+os.system("wget http://services.swpc.noaa.gov/experimental/products/solar-wind/plasma-2-hour.json -O output/plasma-2-hour.json")
+myfile = open("output/plasma-2-hour.json","r")
 j=json.load(myfile)
 
+proj_path = "/home/elfo/GIT/spaceweather/src/spaceweather/"
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "spaceweather.settings")
+sys.path.append(proj_path)
+os.chdir(proj_path)
+
+from core.models import *
 
 for i in j[1:]:
     if i[0][-4] == '0':
-        if None in i:
-            asw = Solarwind(date=i[0],density=0,radialspeed=0,temperature=0,bogus=True)
-        else:
-            asw = Solarwind(date=i[0],density=i[1],radialspeed=i[2],temperature=i[3])
-        asw.save()
+        try:
+            Solarwind.objects.get(date=i[0])
+            print("Data for {} already there!".format(i[0]))
+        except:
+            if None in i:
+                asw = Solarwind(date=i[0],density=0,radialspeed=0,temperature=0,bogus=True)
+            else:
+                asw = Solarwind(date=i[0],density=i[1],radialspeed=i[2],temperature=i[3])
+            print("Data for {} inserted!".format(i[0]))
+            asw.save()
